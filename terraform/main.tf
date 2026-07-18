89fe2e7aadedb0d76ea56de81c0b59a0b415873b
rchirutkar <rchirutkar786@gmail.com>
2026-07-18 12:36:18 +0530

    Add S3 bucket resource for application data

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,8 @@
+resource "aws_s3_bucket" "application_data_bucket" {
+  bucket = var.bucket_name
 
+  tags = {
+    Name        = "${var.aws_region}-${var.project_name}-data"
+    Environment = "Dev"
+  }
+}
