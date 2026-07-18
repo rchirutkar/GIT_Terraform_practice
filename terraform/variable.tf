241f6eedde27dada75fba15f8a328068f71bc12f
rchirutkar <rchirutkar786@gmail.com>
2026-07-18 12:18:53 +0530

    Define repository_suffixes variable in Terraform
    Add variable for ECR repository suffixes with default values.

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,1 +1,5 @@
-
+variable "repository_suffixes" {
+  description = "List of ECR repository suffixes to create (will be prefixed with name_prefix)"
+  type        = list(string)
+  default     = ["app", "api", "worker"]
+}
